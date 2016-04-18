fd16748080cbb588be7163b2b2109c80aecd6755
evanBergeron <bergeronej@gmail.com>
2016-04-18 16:51:02 -0500

    Implement memo_table get, put, and contains_key.
    Untested, uses a kind-of gross key-value pair hack at the moment.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -m64 -O3 -g 
+CXXFLAGS = -std=c++11 -m64 -O3 -g
 
 all:
 	$(CXX) $(CXXFLAGS) -o bdd *.cpp *.h
--- memo_table.cpp
+++ memo_table.cpp
@@ -1,23 +1,69 @@
 #include <cstddef>
+#include <cstdlib>
+#include <tuple>
 #include "bdd.h"
+
+#define TABLE_SIZE 100
+
+struct key_value {
+  std::tuple<bdd_node*, bdd_node*, bdd_node*> key;
+  bdd_node *value;
+};
+
+struct memo_table {
+  int *hash_array;
+  key_value *data_array;
+};
+
+static memo_table *table = (memo_table*)std::malloc(sizeof(memo_table));
+
+int calculate_hash(bdd_node *F, bdd_node *G, bdd_node* H) {
+  return 0;
+}
 
 /**
  * docs yay
  */
 bdd_node *get_result(bdd_node *F, bdd_node *G, bdd_node *H) {
-  return NULL;
+  int hash = calculate_hash(F, G, H);
+  int index = hash % TABLE_SIZE;
+  int cur_hash = table->hash_array[index];
+  if (cur_hash != hash) {
+    return NULL; // TODO this is a bad design decision
+  }
+  std::tuple<bdd_node*, bdd_node*, bdd_node*> key {F, G, H};
+  if (table->data_array[index].key == key) {
+    bdd_node *result = table->data_array[index].value;
+    table->hash_array[index] = hash;
+    return result;
+  } else {
+    table->hash_array[index] = hash;
+    return NULL;
+  }
 }
 
 /**
  * docs yay
  */
 bdd_node *put_result(bdd_node *F, bdd_node *G, bdd_node *H, bdd_node *result) {
-  return NULL;
+  int hash = calculate_hash(F, G, H);
+  int index = hash % TABLE_SIZE;
+  int cur_hash = table->hash_array[index];
+  std::tuple<bdd_node*, bdd_node*, bdd_node*> key {F, G, H};
+  if (cur_hash == hash && table->data_array[index].key == key) {
+    return result;
+  }
+  table->data_array[index].key = key;
+  table->data_array[index].value = result;
+  table->hash_array[index] = hash;
+  return result;
 }
 
 /**
  * docs yay
  */
 bdd_node *contains_key(bdd_node *F, bdd_node *G, bdd_node *H) {
+  int hash = calculate_hash(F, G, H);
+  int index = hash % TABLE_SIZE;
   return NULL;
 }
