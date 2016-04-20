6530db1dfb732ea408e761bb0a7edd385457dae2
Kevin Zheng <sunday0303@gmail.com>
2016-04-20 01:14:34 -0400

    Clean out skeleton for memo table

--- include/memo_table.h
+++ include/memo_table.h
@@ -1,33 +1,26 @@
-#include "bdd.h"
-#include <tuple>
-
 #ifndef MEMO_TABLE_H
 #define MEMO_TABLE_H
 
-struct key_value {
-  std::tuple<bdd_node*, bdd_node*, bdd_node*> key;
-  bdd_node *value;
-};
-
-struct memo_table {
-  int *hash_array;
-  key_value *data_array;
-};
-
-void memo_table_init(int size);
+#include "bdd.h"
 
 /**
- * docs yay
+ * Initialize the memoization table with initial capacity size.
+ */
+void memo_table_init(int capacity);
+
+/**
+ * Search the table for pre-computed results (F,G,H).
+ * Returns a pointer to the bdd node if it was in the table, otherwise nullptr.
  */
 bdd_node *get_result(bdd_node *F, bdd_node *G, bdd_node *H);
 
 /**
- * docs yay
+ * Place a computed result in the memoization table (F,G,H) -> result.
  */
-bdd_node *put_result(bdd_node *F, bdd_node *G, bdd_node *H, bdd_node *result);
+void put_result(bdd_node *F, bdd_node *G, bdd_node *H, bdd_node *result);
 
 /**
- * docs yay
+ * Returns true of the results for (F,G,H) are in the table, else fase.
  */
 bool contains_key(bdd_node *F, bdd_node *G, bdd_node *H);
 
--- src/pbdd/memo_table.cpp
+++ src/pbdd/memo_table.cpp
@@ -1,77 +1,32 @@
-#include <cstddef>
 #include <cstdlib>
-#include <tuple>
 #include "bdd.h"
 
-#define TABLE_SIZE 100
+/**
+ * Initialize the memoization table with initial capacity size.
+ */
+void memo_table_init(int capacity) {
 
-struct key_value {
-  std::tuple<bdd_node*, bdd_node*, bdd_node*> key;
-  bdd_node *value;
-};
-
-struct memo_table {
-  int *hash_array;
-  key_value *data_array;
-};
-
-static memo_table *table;// = (memo_table*)std::malloc(sizeof(memo_table));
-
-int calculate_hash(bdd_node *F, bdd_node *G, bdd_node* H) {
-  return 0;
-}
-
-void memo_table_init(int size){
-  table = (memo_table*)std::malloc(sizeof(memo_table));
-  table->hash_array = (int*)std::malloc(sizeof(int) * size);
-  table->data_array = (key_value*)std::malloc(sizeof(key_value) * size);
 }
 
 /**
- * docs yay
+ * Search the table for pre-computed results (F,G,H).
+ * Returns a pointer to the bdd node if it was in the table, otherwise nullptr.
  */
 bdd_node *get_result(bdd_node *F, bdd_node *G, bdd_node *H) {
-  int hash = calculate_hash(F, G, H);
-  int index = hash % TABLE_SIZE;
-  int cur_hash = table->hash_array[index];
-  if (cur_hash != hash) {
-    return NULL; // TODO this is a bad design decision
-  }
-  std::tuple<bdd_node*, bdd_node*, bdd_node*> key {F, G, H};
-  if (table->data_array[index].key == key) {
-    bdd_node *result = table->data_array[index].value;
-    table->hash_array[index] = hash;
-    return result;
-  } else {
-    table->hash_array[index] = hash;
-    return NULL;
-  }
+  return nullptr;
 }
 
 /**
- * docs yay
+ * Place a computed result in the memoization table (F,G,H) -> result.
  */
-bdd_node *put_result(bdd_node *F, bdd_node *G, bdd_node *H, bdd_node *result) {
-  int hash = calculate_hash(F, G, H);
-  int index = hash % TABLE_SIZE;
-  int cur_hash = table->hash_array[index];
-  std::tuple<bdd_node*, bdd_node*, bdd_node*> key {F, G, H};
-  if (cur_hash == hash && table->data_array[index].key == key) {
-    return result;
-  }
-  table->data_array[index].key = key;
-  table->data_array[index].value = result;
-  table->hash_array[index] = hash;
-  return result;
+void put_result(bdd_node *F, bdd_node *G, bdd_node *H, bdd_node *result) {
+
 }
 
 /**
- * docs yay
+ * Returns true of the results for (F,G,H) are in the table, else fase.
  */
 bool contains_key(bdd_node *F, bdd_node *G, bdd_node *H) {
-  int hash = calculate_hash(F, G, H);
-  int index = hash % TABLE_SIZE;
-  int cur_hash = table->hash_array[index];
-  std::tuple<bdd_node*, bdd_node*, bdd_node*> key {F, G, H};
-  return (cur_hash == hash && table->data_array[index].key == key);
+  return false;
 }
+
