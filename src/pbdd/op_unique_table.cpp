e555967306acba25bcfa16ab6a91aa08ebff8104
evanBergeron <bergeronej@gmail.com>
2016-05-04 00:14:57 -0500

    Fix argument ordering in op unique table

--- src/pbdd/op_unique_table.cpp
+++ src/pbdd/op_unique_table.cpp
@@ -4,10 +4,10 @@ OpUniqueTable::OpUniqueTable(int maxnodes) {
   internal = new HashTable(maxnodes);
 }
 
-op_node *OpUniqueTable::lookup(bool_op op, bdd_node *hi, bdd_node *lo) {
-  return (op_node*)internal->lookup(op, hi, lo);
+op_node *OpUniqueTable::lookup(bool_op op, bdd_node *f, bdd_node *g) {
+  return (op_node*)internal->lookup(op, g, f);
 }
 
-op_node *OpUniqueTable::lookup_or_insert(bool_op op, bdd_node *hi, bdd_node *lo) {
-  return (op_node*)internal->lookup_or_insert(op, hi, lo);
+op_node *OpUniqueTable::lookup_or_insert(bool_op op, bdd_node *f, bdd_node *g) {
+  return (op_node*)internal->lookup_or_insert(op, g, f);
 }
