d2fb451fd9390f4e59c4b60dd8743791f63409f1
Kevin Zheng <sunday0303@gmail.com>
2016-05-07 04:41:43 -0400

    Expose all requests

--- include/bfs_reqs.h
+++ include/bfs_reqs.h
@@ -5,31 +5,40 @@
 #include "nodemanager.h"
 #include "bdd.h"
 
-/**
- * Variable requests for all levels.
- * bdd_ptr can point to a specific request.
- */
-struct var_reqs {
-  req *requests;      // Array of requests
-  uint32_t length;    // Length of the array
-};
+typedef bdd_ptr req_ptr;
 
 /**
  * A single ITE request. ITE(f,g,h), with the result
  * pre-allocated as result
  */
 struct req {
-  bdd_ptr_packed f;
-  bdd_ptr_packed g;
-  bdd_ptr_packed h;
-  bdd result;
+  bdd_ptr_packed f;   // pointer to actual bdd node
+  bdd_ptr_packed g;   // pointer to actual bdd node
+  bdd_ptr_packed h;   // pointer to actual bdd node
+  bdd result;         // temp bdd node
 };
 
+/**
+ * Variable requests for an individual level.
+ */
+struct var_reqs {
+  req *requests;      // Array of requests
+  uint32_t capacity;  // Length of the array
+  uint32_t numnodes;  // Number of requests in the array
+};
+
+struct all_reqs {
+  var_reqs *reqs;
+  uint32_t numvars;
+};
+
+extern all_reqs requests;
+
 /** Convert a bfs request pointer to a C pointer */
-req *reqptr2cptr(bdd_ptr &ptr);
+req *reqptr2cptr(req_ptr &ptr);
 
 /** Convert a bfs request pointer to a pointer to the result bdd node */
-bddptr reqptr2resptr(bdd_ptr &ptr);
+bdd_ptr reqptr2resptr(req_ptr &ptr);
 
 /** Initialize the BDD queues */
 void bfs_reqs_init(uint16_t numvars);
@@ -38,6 +47,6 @@ void bfs_reqs_init(uint16_t numvars);
 void bfs_reqs_free();
 
 /** Get a cannonical pointer to a request */
-bdd_ptr bfs_reqs_lookup_or_insert(bdd_ptr f, bdd_ptr g, bdd_ptr h);
+req_ptr bfs_reqs_lookup_or_insert(bdd_ptr f, bdd_ptr g, bdd_ptr h);
 
 #endif /* BFS_REQS */
